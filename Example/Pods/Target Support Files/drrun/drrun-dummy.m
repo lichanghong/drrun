5dae5c74694d3e0b5d6781d416d45ca100356cd4
lichanghong <lichanghong@soyoung.com>
2018-08-09 19:14:34 +0800

    c

---
+++ Example/Pods/Target Support Files/drrun/drrun-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_drrun : NSObject
+@end
+@implementation PodsDummy_drrun
+@end
---
+++ drrun/Classes/DRRun.m
@@ -0,0 +1,16 @@
+//
+//  DRRun.m
+//  tableviewanimation
+//
+//  Created by lichanghong on 8/9/18.
+//  Copyright © 2018 lichanghong. All rights reserved.
+//
+
+#import "DRRun.h"
+
+@implementation DRRun
+- (void)run
+{
+    NSLog(@"this is debug");
+}
+@end
